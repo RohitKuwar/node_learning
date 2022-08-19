e4e87b94ff2b1335b9cea1e9bb802f8589b6b595
RohitKuwar <rohit1kuwar@gmail.com>
2022-08-19 13:00:26 +0530

    node learning - routing

--- server.js
+++ server.js
@@ -6,21 +6,21 @@ const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
   //set header content type
-  res.setHeader("Content-Type", "text/plain");
-  res.write("Hello world!!!");
-  res.end();
+  // res.setHeader("Content-Type", "text/plain");
+  // res.write("Hello world!!!");
+  // res.end();
 
   //send html page
-  // res.setHeader("Content-Type", "text/html");
-  // fs.readFile('./views/index.html', (err, data) => {
-  //   if(err) {
-  //     console.log(err);
-  //     res.end();
-  //   } else {
-  //     res.write(data);
-  //     res.end();
-  //   }
-  // })
+  res.setHeader("Content-Type", "text/html");
+  fs.readFile('./views/index.html', (err, data) => {
+    if(err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  })
 });
 
 server.listen(3000, 'localhost', () => {
