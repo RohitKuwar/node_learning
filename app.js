2d7c3f96e8e00542013e3853ffeac1e01cdaeef2
RohitKuwar <rohit1kuwar@gmail.com>
2022-08-19 11:36:51 +0530

    node learning - modules

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const dotenv = require('dotenv');
 const { environment } = require('./config')
 const hostname = '127.0.0.1';
 const port = 3000;
+const os = require('os');
+const {people, ages} = require('./people')
+const file = require('./filesystem')
 
-//environment varialbles in 
+//environment varialbles in app.js
 dotenv.config();
 
 const server = http.createServer((req, res) => {
@@ -18,3 +21,20 @@ server.listen(port, hostname, () => {
   console.log(`user id: ${process.env.USER_ID}`);
   console.log(`environment: ${environment}`);
 });
+
+// console.log(global);
+// setTimeout(() => {
+//   console.log('in the setTimeout after 3000ms');
+//   clearInterval(inte);
+//   console.log('clear');
+// }, 3000);
+
+// const inte = setInterval(() => {
+//   console.log("setInterval 1000ms");
+// }, 1000);
+
+console.log("os:", os.platform(), os.homedir());
+console.log("dirname:", __dirname);
+console.log("filename:", __filename);
+
+console.log("people file:", people, ages);
---
+++ people.js
@@ -0,0 +1,10 @@
+const people = ['swarup', 'somesh', 'ganesh', 'abhishek'];
+
+const ages = [24, 20, 32, 26];
+
+console.log('peopleJS', people);
+
+module.exports = {
+  people,
+  ages
+}
