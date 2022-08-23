da0fba2e563be15766961945282322ef4b0f7de5
RohitKuwar <rohit1kuwar@gmail.com>
2022-08-23 18:14:38 +0530

    express learning -ejs view engine

--- express/app.js
+++ express/app.js
@@ -3,11 +3,33 @@ const express = require('express');
 //express app
 const app = express();
 
+//register view engine
+app.set('view engine', 'ejs');
+
 //listen for requests
 app.listen(8000);
 
 app.get('/', (req, res) => {
-  res.sendFile('./views/index.html', {root: __dirname});
+  // res.sendFile('./views/index.html', {root: __dirname});
+  
+  //here we are rendering html page using ejs view engine
+  //we are passing data to index.ejs file using object like syntax
+  res.render('index', {title: 'Home', number: '100'});
+});
+
+//here we are rendering html page using ejs view engine
+app.get('/blog', (req, res) => {
+  const blogs = [
+    {title: 'Blog1', text: 'This is blog1.'},
+    {title: 'Blog2', text: 'This is blog2.'},
+    {title: 'Blog3', text: 'This is blog3.'},
+  ];
+
+  res.render('blog', {title: 'Blog', blogs: blogs});
+});
+
+app.get('/blog/create', (req, res) => {
+  res.render('create', {title: 'Create'});
 });
 
 app.get("/about", (req, res) => {
