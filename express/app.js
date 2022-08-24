fc4d12a2752d1c59faa8a37c7634a99a1515b751
RohitKuwar <rohit1kuwar@gmail.com>
2022-08-24 09:39:31 +0530

    express learning - middleware

--- express/app.js
+++ express/app.js
@@ -9,12 +9,32 @@ app.set('view engine', 'ejs');
 //listen for requests
 app.listen(8000);
 
+//middleware - It is the code running on the server in between request and response process
+app.use((req, res, next) => {
+  console.log('new request made');
+  console.log('host:', req.hostname);
+  console.log('path:', req.path);
+  console.log('method:', req.method);
+  next();       //It tells express to move to next middleware
+});
+
+app.use((req, res, next) => {
+  console.log('next middleware');
+  next();
+});
+
 app.get('/', (req, res) => {
   // res.sendFile('./views/index.html', {root: __dirname});
   
   //here we are rendering html page using ejs view engine
   //we are passing data to index.ejs file using object like syntax
   res.render('index', {title: 'Home', number: '100'});
+});
+
+//As we have placed below middleware after home page request code so it will not run on home page request. Hence order and place of middleware matters.
+app.use((req, res, next) => {
+  console.log('next middleware after home page');
+  next();
 });
 
 //here we are rendering html page using ejs view engine
